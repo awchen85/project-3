d6cfc0a7f39093fcd8a294ef269902a9d1567ea5
Mitchell Armstrong <mra0211@gmail.com>
2023-01-03 21:22:55 -0500

    Created an animation to the name in the nav bar

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
   name: {
@@ -13,44 +13,47 @@ const userSchema = new Schema({
     type: String,
     required: true,
     unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
   },
   phone: {
     type: String,
     required: true,
     minlength: 10,
     unique: true,
-    match: [/.+@.+\..+/, 'Must match an email address!']
   },
-  //min 7 letter password, symbol, uppercase, lowercase, and a number
+  // min 7 letter password, symbol, uppercase, lowercase, and a number
   password: {
     type: String,
     required: true,
     minlength: 7,
-    match: [/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{7,}$/, 'Must meet requirements!']
+    match: [
+      /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{7,}$/,
+      'Must meet requirements!',
+    ],
   },
   likes: [
     {
       type: Schema.Types.ObjectId,
-      ref: "Room",
+      ref: 'Room',
     },
   ],
   rooms: [
     {
       type: Schema.Types.ObjectId,
-      ref: "Room",
+      ref: 'Room',
     },
   ],
   reviews: [
     {
       type: Schema.Types.ObjectId,
-      ref: "Review",
+      ref: 'Review',
     },
   ],
 });
 
 // set up pre-save middleware to create password
-userSchema.pre("save", async function (next) {
-  if (this.isNew || this.isModified("password")) {
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
@@ -63,6 +66,6 @@ userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
