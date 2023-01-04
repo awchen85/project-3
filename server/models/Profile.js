c84b60c821949cf039bbb1fa179083973ff3d6cb
Patrick M <patrickmacdonald07@gmail.com>
2023-01-03 20:16:55 -0500

    stashing profile model

---
+++ server/models/Profile.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const profileSchema = new Schema({
+  age: {
+    type: Number,
+    trim: true,
+  },
+  gender: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  budget: {
+    type: Number,
+    required: true,
+  },
+  location: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  aboutMe: {
+    type: String,
+    trim: true,
+    minlength: 1,
+    maxlength: 500,
+  },
+  allowPets: {
+    type: Boolean,
+    required: true,
+  },
+  allowChildren: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const Profile = mongoose.model('Profile', profileSchema);
+
+module.exports = Profile;
--- server/models/User.js
+++ server/models/User.js
@@ -7,25 +7,28 @@ const userSchema = new Schema({
   firstName: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   lastName: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   email: {
     type: String,
     required: true,
     unique: true,
-    match: [/.+@.+\..+/, 'Must match an email address!']
+    match: [/.+@.+\..+/, 'Must match an email address!'],
   },
   //min 7 letter password, symbol, uppercase, lowercase, and a number
   password: {
     type: String,
     required: true,
     minlength: 7,
-    match: [/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{7,}$/, 'Must meet requirements!']
+    match: [
+      /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{7,}$/,
+      'Must meet requirements!',
+    ],
   },
 });
 
