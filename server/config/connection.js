08ed75a2cece96b68dfeaa0d8ef2d5520cf0e015
Mitchell Armstrong <mra0211@gmail.com>
2023-01-10 22:14:00 -0500

    Changed db name in the connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/project-3-db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Our-Place',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 module.exports = mongoose.connection;
